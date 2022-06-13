9cfee0ef1bea19edaa4fa266057f9b56da067742
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-06-13 15:40:13 -0400

    Connected server.js to routers. Middleware set up.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,26 @@
+//this produces the token in POSTMAN.
 const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+    //get token from headers object in Postman after you create and send a new user or blog. Click on the headers, x-auth-token goes in 'key', then copy and past the hash token in 'value'.
+    const token = req.header('x-auth-token')
+
+    // IF NO TOKEN IS DEFINED ABOVE
+    IF(!token) {
+        return res.json('NO TOKEN, ACCESS DENIED')
+    }
+    // IF WE HAVE A TOKEN PROCESS IT USING SECRET_KEY USING DOTENV (.env file)
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+
+        console.log(decoded)
+
+        next()
+    } catch (error) {
+        console.log(error)
+        res.status(400).json('TOKEN NOT VALID')
+    }
+
+
+
+}
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
+//set up the npms
 const express = require('express')
 const app = express()
-const PORT = 3000
 const morgan = require('morgan')
 const helmet = require('helmet')
 
+// INITIALIZE DOTENV WHICH CONNECTS MONGODB TO MONGOOSE MODELS AND GETS THE ACCESS TOKEN FOR NEW USERS AND WHEN USERS LOGIN
+require('dotenv').config()
 
-app.get('/',(req,res)=>{
+//connect the configuration of mongoose to mongoDB and then to this server which is connected to all the routers, which are in turn connected to all the models. This file is the API's GRAND CENTRAL STATION
+const mongoConfig = require('./config/mongoConfig')
+
+//CONNECT ALL THE ROUTERS
+const userRouter = require('./routers/userRouter')
+const blogRouter = require('./routers/blogRouter')
+const authRouter = require('./routers/authRouter')
+
+//USE MIDDLEWARE i need to research each one and leave a comment on each of them here.
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(helmet())
+
+
+//set up the LOCALHOST PORT 
+const PORT = 3000
+
+// app.use mounts the middleware functions which these routes hold. These are the trains that enter Grand Central station with their goods.
+app.use('/users', userRouter)
+app.use('/blogs', blogRouter)
+app.use('/auth', authRouter)
+
+//Routes HTTP GET requests to the specified path with the specified callback functions.
+app.get('/', (req, res) => {
     res.status(200).json({
         message: 'API is up'
     })
